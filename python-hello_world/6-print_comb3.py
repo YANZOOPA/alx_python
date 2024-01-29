7439f817d4db10a3e63edc3b52f3cb56d966d9b1
YANZOOPA <yahia.ni.19@gmail.com>
2024-01-29 18:08:08 +0100

    fx

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,4 @@
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:1d}{:1d}".format(i, j), end=", ")
+        print("{:1d}{:1d}".format(i, j), end=", " if i < 8 else "\n")
 print()
