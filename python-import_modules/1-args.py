7b9e36714c50172dc992dbf4b1e8c2decf6f2945
YANZOOPA <yahia.ni.19@gmail.com>
2024-01-30 15:03:23 +0100

    1

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv[1:]
+    num_args = len(args)
+
+    print("{:d} argument{}{}{}".format(
+        num_args,
+        's' if num_args != 1 else '',
+        ':' if num_args > 0 else '.',
+        '\n' if num_args > 0 else ''
+    ))
+
+    for i, arg in enumerate(args, start=1):
+        print("{:d}: {}".format(i, arg))
