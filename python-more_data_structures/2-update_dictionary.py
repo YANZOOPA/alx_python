4ea0a20a69083405986a84fb4949bd8988c31c9e
YANZOOPA <yahia.ni.19@gmail.com>
2024-01-30 16:14:13 +0100

    2fx

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,3 +1,3 @@
-def update_dictionary(a_dictionary, key, value):
-    # Update the value for the given key if it exists, otherwise add a new key/value pair
-    a_dictionary[key] = value
+def print_dictionary(a_dictionary):
+    for key, value in a_dictionary.items():
+        print(f'{key}: {value}')
