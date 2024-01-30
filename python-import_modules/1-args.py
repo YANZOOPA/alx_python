955a2c258b5e27e18826131d39b23f6dbea7ea54
YANZOOPA <yahia.ni.19@gmail.com>
2024-01-30 15:26:50 +0100

    1a

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,16 +1,14 @@
-#!/usr/bin/python3
+import sys
+
+def print_arguments():
+    num_args = len(sys.argv) - 1  # Subtract 1 to exclude the script name
+    plural_s = 's' if num_args != 1 else ''
+    colon_or_dot = ':' if num_args > 0 else '.'
+
+    print(f"Number of argument{plural_s}: {num_args}, followed by {colon_or_dot}")
+
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print(f"{i}: {arg}")
+
 if __name__ == "__main__":
-    import sys
-
-    args = sys.argv[1:]
-    num_args = len(args)
-
-    print("{:d} argument{}{}{}".format(
-        num_args,
-        's' if num_args != 1 else '',
-        ':' if num_args > 0 else '.',
-        '\n' if num_args > 0 else ''
-    ))
-
-    for i, arg in enumerate(args, start=1):
-        print("{:d}: {}".format(i, arg))
+    print_arguments()
