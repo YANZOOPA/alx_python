004c686f007a19007545e5b2ccd64ba7e478eaa9
YANZOOPA <yahia.ni.19@gmail.com>
2024-01-30 16:39:02 +0100

    gp2

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,2 +1,14 @@
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    # Create a new dictionary with the original content
+    new_dict = dict(a_dictionary)
+
+    # Update the value for the given key in the new dictionary
+    new_dict[key] = value
+
+    return new_dict
+
+def print_sorted_dictionary(my_dict):
+    """Print sorted dictionary"""
+    keys = sorted(my_dict.keys())
+    for k in keys:
+        print("{}: {}".format(k, my_dict[k]))
