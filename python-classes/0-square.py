ad3ea96898bef7792cf1eda3f780ef13ddae4a05
YANZOOPA <yahia.ni.19@gmail.com>
2024-02-12 22:30:20 +0100

    ?

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,6 @@
+#python3 -c 'print(__import__("my_module").__doc__)'
 class Square:
+    #python3 -c 'print(__import__("my_module").MyClass.__doc__)'
     """
     This class represents a square.
 
@@ -6,7 +8,9 @@ class Square:
         __size (int): The size of the square.
     """
 
-    def __init__(self, size=0):
+    def __init__(self, size):
+        #python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        #python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)')
         """
         Initializes a new instance of the Square class.
 
@@ -17,9 +21,4 @@ class Square:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
