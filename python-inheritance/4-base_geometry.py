26fb1699ea63d91f2f1de2449a8c18c46bc10155
YANZOOPA <yahia.ni.19@gmail.com>
2024-02-20 15:25:56 +0100

    4f

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -9,3 +9,12 @@ class BaseGeometry:
         Raises an Exception with the message 'area() is not implemented'.
         """
         raise Exception("area() is not implemented")
+
+    def __dir__(self):
+        """
+        Returns the attributes of the object.
+
+        :return: List of attributes.
+        :rtype: list
+        """
+        return [attr for attr in dir(self) if not callable(getattr(self, attr)) and not attr.startswith("__")]
