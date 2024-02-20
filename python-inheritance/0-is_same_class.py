63ab22dfe7ba8150216fc534f2e6f137e32c261d
YANZOOPA <yahia.ni.19@gmail.com>
2024-02-20 15:02:38 +0100

    01

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,3 +1,4 @@
+"""writing a fonction  for the task Exact same object """
 def is_same_class(obj, a_class):
     """
     Returns True if the object is exactly an instance of the specified class; otherwise False.
