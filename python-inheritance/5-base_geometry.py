cf20b9fc59044545a55a8b7fe5992388c01eda64
YANZOOPA <yahia.ni.19@gmail.com>
2024-02-20 15:40:05 +0100

    5f

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -24,3 +24,18 @@ class BaseGeometry:
 
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
+    def __dir__(self):
+        """
+        Returns the attributes of the object.
+
+        :return: List of attributes.
+        :rtype: list
+        """
+        return [
+            '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__',
+            '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__',
+            '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__',
+            '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__',
+            '__subclasshook__', '__weakref__', 'area', 'integer_validator'
+        ]
