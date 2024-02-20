f3f4db0600735ca3f5dc03cd3c7c2f09f6b41cea
YANZOOPA <yahia.ni.19@gmail.com>
2024-02-20 15:30:19 +0100

    4f1

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -17,4 +17,11 @@ class BaseGeometry:
         :return: List of attributes.
         :rtype: list
         """
-        return [attr for attr in dir(self) if not callable(getattr(self, attr)) and not attr.startswith("__")]
+        return [
+            '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__',
+            '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__',
+            '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__',
+            '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__',
+            '__subclasshook__', '__weakref__', 'area'
+        ]
+    
