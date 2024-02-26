ed8a1d760262fee30dfc25502564bb0a4b4c52ec
YANZOOPA <yahia.ni.19@gmail.com>
2024-02-26 17:30:15 +0100

    0.1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,3 +20,19 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    @staticmethod
+    def integer_validator(name, value):
+        """
+        Validates the value to be an integer greater than 0.
+
+        :param name: The name of the value.
+        :param value: The value to be validated.
+        :raises TypeError: If the value is not an integer.
+        :raises ValueError: If the value is less or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
