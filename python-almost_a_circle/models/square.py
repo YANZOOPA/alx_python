fcfb0cb500767d686d49433761e66e68b465d5d6
YANZOOPA <yahia.ni.19@gmail.com>
2024-02-26 17:51:50 +0100

    10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -19,10 +19,13 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
-    def __str__(self):
-        """
-        Return a string representation of the Square.
+    @property
+    def size(self):
+        """Getter for size."""
+        return self.width
 
-        :return: String representation of the Square.
-        """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+    @size.setter
+    def size(self, value):
+        """Setter for size."""
+        self.width = value
+        self.height = value
