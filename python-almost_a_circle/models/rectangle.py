9ab68d9579d5c27fbc10fb3d60e2c8cb6fddc968
YANZOOPA <yahia.ni.19@gmail.com>
2024-02-26 17:45:30 +0100

    6

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -87,12 +87,12 @@ class Rectangle(Base):
 
     def display(self):
         """
-        Print the Rectangle instance with the character #.
-
-        Note: This method does not handle x and y.
+        Print the Rectangle instance with the character #, taking care of x and y.
         """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """
