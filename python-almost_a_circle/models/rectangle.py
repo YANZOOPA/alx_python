12d4d1452ba5695bae3240f7384f257d703de3db
YANZOOPA <yahia.ni.19@gmail.com>
2024-02-26 17:27:16 +0100

    1

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,70 @@
+"""
+Module for the Rectangle class.
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    A class representing a rectangle.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes the Rectangle class.
+
+        :param width: The width of the rectangle.
+        :param height: The height of the rectangle.
+        :param x: The x-coordinate of the rectangle (default is 0).
+        :param y: The y-coordinate of the rectangle (default is 0).
+        :param id: The id of the rectangle (default is None).
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter for width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width."""
+        self.integer_validator("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height."""
+        self.integer_validator("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for x."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for x."""
+        self.integer_validator("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for y."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter for y."""
+        self.integer_validator("y", value)
+        self.__y = value
